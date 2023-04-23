435d9f843260212bbcb162cad8a3afcbf7cff579
Euler Filho <euler-reis-das-virgens.filho@capgemini.com>
2023-04-23 14:01:36 -0300

    add theme and Typhograpy component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import intelbrasIcon from "/intelbras.ico";
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
 import { useDeviceFetcher } from "../src/services/Devices";
-import "./App.css";
+import intelbrasIcon from "/intelbras.ico";
+import theme from "./theme";
 
 const App = () => {
   const { device, loading } = useDeviceFetcher();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div>
         <a>
           <img src={intelbrasIcon} className="logo" alt="Intelbras logo" />
@@ -15,7 +17,7 @@ const App = () => {
       <h1>Device Manager</h1>
       <button>refetch</button>
       {!loading && <div>{device}</div>}
-    </>
+    </ThemeProvider>
   );
 };
 
