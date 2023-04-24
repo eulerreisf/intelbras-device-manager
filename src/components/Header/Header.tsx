a0b0b7913bececfc2724f0619fc58ac4201e4f43
Euler Filho <euler-reis-das-virgens.filho@capgemini.com>
2023-04-23 22:03:06 -0300

    finishing header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,59 +1,29 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+import { AppBar, Box, Toolbar, IconButton } from "@mui/material";
 import Logout from "@mui/icons-material/Logout";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
+import { useLogoutFetcher } from "../../services";
+import { IntelbrasLogo } from "../../components";
 
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+const Header = () => {
+  const { logout } = useLogoutFetcher();
 
-import Button from "../Button";
-
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
-function Header() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleLogout = async () => {
+    await logout();
+    //TODO
+    //set authenticated false
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Logo
-          </Typography>
-          <IconButton color="info" aria-label="logout">
+          <IntelbrasLogo />
+          <IconButton color="info" aria-label="logout" onClick={handleLogout}>
             <Logout />
           </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 export default Header;
